3e1979d6f26b4489591c8b6d462b1d45bb0e17dc
chetanCurefit <chetan.kumar@v.curefit.com>
2019-07-10 15:21:49 +0530

    changed constants

--- zipf/zipf-server/constants.js
+++ zipf/zipf-server/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    WEBSITE_TO_CRAWL: 'https://www.google.com/search?ei=zLAlXen5FZP5rQGw1aD4AQ&q=hello&oq=hello&gs_l=psy-ab.3..0i67l6j0j0i67l3.9194.9699..9970...0.0..0.178.641.0j4......0....1..gws-wiz.......0i131i67.Q8-1YiOO9Pw',
+    WEBSITE_TO_CRAWL: 'https://www.wikipedia.org/',
     SOCKET_PORT: 8080,
     HOSTNAME: 'localhost',
     WAIT_TIME_BEFORE_NEXT_POLL_MS: 3000
