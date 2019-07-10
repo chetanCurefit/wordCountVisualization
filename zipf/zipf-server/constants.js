330c326665f9b20a245b9efd1c6afb1e1fe34391
chetanCurefit <chetan.kumar@v.curefit.com>
2019-07-10 15:44:25 +0530

    fixed crawl url

--- zipf/zipf-server/constants.js
+++ zipf/zipf-server/constants.js
@@ -1,5 +1,5 @@
 module.exports = {
-    WEBSITE_TO_CRAWL: 'https://www.wikipedia.org/',
+    WEBSITE_TO_CRAWL: 'https://www.wikipedia.org',
     SOCKET_PORT: 8080,
     HOSTNAME: 'localhost',
     WAIT_TIME_BEFORE_NEXT_POLL_MS: 3000
